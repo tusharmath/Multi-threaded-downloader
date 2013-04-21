db0fd1332d3c9375273345e2829b42a0c57bd853
Tushar Mathur <tusharmath@gmail.com>
2013-04-21 16:51:57 +0530

    enabling debugger

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 var _options = {
-	packageFile: './bin/mt-downloader.app/Contents/Resources/app.nw'
-};
+	packageFile: './bin/app.nw',
+	applicationPath: './bin/mt-downloader.app/Contents/Resources'
 
+};
 var _config = {
 	zip: {
 		createPackage: {
@@ -9,7 +10,19 @@ var _config = {
 				'./ui/*',
 				'./lib',
 				'Package.json'],
-			dest: _options.applicationPath
+			dest: _options.packageFile
+		}
+	},
+	copy: {
+		copyPackage: {
+			files: [{
+				src: [_options.packageFile],
+				//dest:'/Users/tusharmathur/Desktop/Temp/node-webkit-v0.5.0-osx-ia32/node-webkit.app/Contents/Resources',
+				dest: _options.applicationPath,
+				filter: 'isFile',
+				expand: true,
+				flatten: true
+			}]
 		}
 	}
 };
@@ -17,7 +30,7 @@ var _config = {
 module.exports = function(grunt) {
 	grunt.initConfig(_config);
 	grunt.loadNpmTasks('grunt-zip');
-	//grunt.loadNpmTasks('grunt-contrib-copy');
-	//grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['zip:createPackage']);
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['zip:createPackage', 'copy:copyPackage']);
 };
