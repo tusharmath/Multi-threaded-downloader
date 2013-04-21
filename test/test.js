141995a712bfd2e08d8754f8bfdfdd074ffe0c95
Tushar Mathur <tusharmath@gmail.com>
2013-04-21 12:20:36 +0530

    removing status - going for node-webkit

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var nd = require('../lib/ndRequest');
 var options = {
 	path: './Temp/Download.png',
 	url: 'http://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png',
-	threads: 4
+	threadCount: 4
 
 };
 
@@ -11,7 +11,7 @@ var options = {
 var _options = {
 	path: './Temp/VirtualBox-4.2.12-84980-OSX.dmg',
 	url: 'http://dlc.sun.com.edgesuite.net/virtualbox/4.2.12/VirtualBox-4.2.12-84980-OSX.dmg',
-	threads: 4
+	threadCount: 4
 
 };
 
