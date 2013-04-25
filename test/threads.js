2b10e52626650940371ddd247f3504dffb9dce82
Tushar Mathur <tusharmath@gmail.com>
2013-04-25 18:57:26 +0530

    adding nd Status running all unit tests

--- test/threads.js
+++ test/threads.js
@@ -1,15 +1,17 @@
 var ndthreads = require('../lib/core/ndThreads');
 
-var options = {
-	threadCount: 5,
-	fileSize: 100,
-	increasing: true
 
-};
 
-var threader = new ndthreads(options);
+exports.createThreads_inc_lin = function(test) {
 
-exports.createThreads_inc = function(test) {
+	var options = {
+		threadCount: 5,
+		fileSize: 100,
+		type: 'lin'
+
+	};
+	var threader = new ndthreads(options);
+
 	test.expect(11);
 
 	var threads = threader.createThreads();
@@ -18,21 +20,95 @@ exports.createThreads_inc = function(test) {
 
 	test.equal(status.length, 5);
 
-	test.equal(status[0].start, 50);
-	test.equal(status[0].end, 100);
+	test.equal(status[4].start, 57);
+	test.equal(status[4].end, 100);
 
-	test.equal(status[1].start, 25);
-	test.equal(status[1].end, 49);
+	test.equal(status[3].start, 29);
+	test.equal(status[3].end, 56);
+
+	test.equal(status[2].start, 15);
+	test.equal(status[2].end, 28);
+
+	test.equal(status[1].start, 8);
+	test.equal(status[1].end, 14);
+
+
+	test.equal(status[0].start, 0);
+	test.equal(status[0].end, 7);
+
+	test.done();
+};
+
+exports.createThreads_inc_fib = function(test) {
+
+	var options = {
+		threadCount: 5,
+		fileSize: 100,
+		type: 'fib'
+
+	};
+	var threader = new ndthreads(options);
+
+	test.expect(11);
+
+	var threads = threader.createThreads();
+
+	var status = threads.getStatus();
+
+	test.equal(status.length, 5);
+
+	test.equal(status[4].start, 50);
+	test.equal(status[4].end, 100);
+
+	test.equal(status[3].start, 25);
+	test.equal(status[3].end, 49);
 
 	test.equal(status[2].start, 12);
 	test.equal(status[2].end, 24);
 
-	test.equal(status[3].start, 6);
-	test.equal(status[3].end, 11);
+	test.equal(status[1].start, 6);
+	test.equal(status[1].end, 11);
 
 
-	test.equal(status[4].start, 0);
-	test.equal(status[4].end, 5);
+	test.equal(status[0].start, 0);
+	test.equal(status[0].end, 5);
+
+	test.done();
+};
+
+
+exports.createThreads = function(test) {
+
+	var options = {
+		threadCount: 5,
+		fileSize: 100
+
+	};
+	var threader = new ndthreads(options);
+
+	test.expect(11);
+
+	var threads = threader.createThreads();
+
+	var status = threads.getStatus();
+
+	test.equal(status.length, 5);
+
+	test.equal(status[0].start, 0);
+	test.equal(status[0].end, 20);
+
+	test.equal(status[1].start, 21);
+	test.equal(status[1].end, 40);
+
+	test.equal(status[2].start, 41);
+	test.equal(status[2].end, 60);
+
+	test.equal(status[3].start, 61);
+	test.equal(status[3].end, 80);
+
+
+	test.equal(status[4].start, 81);
+	test.equal(status[4].end, 100);
 
 	test.done();
 };
