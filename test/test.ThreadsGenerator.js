3d438134bbabe3d422adf7490f1005eae9f6e177
Tushar Mathur <tusharmath@gmail.com>
2013-06-01 13:23:40 +0530

    ThreadsGenerator tests updated

--- test/test.ThreadsGenerator.js
+++ test/test.ThreadsGenerator.js
@@ -1,88 +1,86 @@
 var should = require('should');
-var ThreadsGenerator = require('../lib/core/ThreadsGenerator');
+var Factory = require('../lib/utils/Factory');
+Factory.register({
+	ThreadsGenerator: {
+		isSingleton: true
+
+	}
+});
+
+
 
 describe('Module: ThreadsGenerator', function() {
-	afterEach(function() {
-		ThreadsGenerator.destroy = true;
-	});
 
-	it('should be an object', function() {
-		ThreadsGenerator.should.be.a('object');
-	});
 
 	describe('properties', function() {
 
-		var properties = ['threads', 'fileSize', 'blockSize', 'count', 'create', 'destroy', 'states'];
-		ThreadsGenerator.create({});
+		var properties = ['threads', 'fileSize', 'blockSize', 'count', 'states'];
+		var threadsGenerator = Factory.create('ThreadsGenerator');
+
 		it('should have properties - ' + properties.join(', '), function() {
 			properties.forEach(function(p) {
-				ThreadsGenerator.should.have.property(p);
+				threadsGenerator.should.have.property(p);
 			});
 		});
 
 
 	});
 
-	it('should be of singleton type', function() {
-		ThreadsGenerator.create({});
-		var t1 = ThreadsGenerator;
-		ThreadsGenerator.create({});
-		var t2 = ThreadsGenerator;
-		t1.should.equal(t2);
-
-	});
 
 
 	it('should have valid properties after initialization', function() {
-		ThreadsGenerator.create({
+		var threadsGenerator = Factory.create('ThreadsGenerator', {
 			count: 4,
 			fileSize: 400
-		});
-		ThreadsGenerator.threads.should.have.lengthOf(4);
-		ThreadsGenerator.count.should.equal(4);
-		ThreadsGenerator.fileSize.should.equal(400);
-		ThreadsGenerator.blockSize.should.equal(100);
+		}, true);
+
+		threadsGenerator.threads.should.have.lengthOf(4);
+		threadsGenerator.count.should.equal(4);
+		threadsGenerator.fileSize.should.equal(400);
+		threadsGenerator.blockSize.should.equal(100);
 
 	});
 
 	it('should have thread states', function() {
-		ThreadsGenerator.states.should.have.keys('open', 'closed', 'failed');
+		var threadsGenerator = Factory.create('ThreadsGenerator');
+		threadsGenerator.states.should.have.keys('open', 'closed', 'failed');
 	});
 
 	it('should match thread specs', function() {
-		var threadsGenerator = ThreadsGenerator.create({
+
+		var threadsGenerator = Factory.create('ThreadsGenerator', {
 			count: 4,
 			fileSize: 401
-		});
+		}, true);
 
 		//Thread 0
-		ThreadsGenerator.threads[0].start.should.equal(0);
-		ThreadsGenerator.threads[0].position.should.equal(0);
-		ThreadsGenerator.threads[0].end.should.equal(101);
-		ThreadsGenerator.threads[0].connection.should.equal('open');
-		ThreadsGenerator.threads[0].header.should.equal('bytes=0-101');
+		threadsGenerator.threads[0].start.should.equal(0);
+		threadsGenerator.threads[0].position.should.equal(0);
+		threadsGenerator.threads[0].end.should.equal(101);
+		threadsGenerator.threads[0].connection.should.equal('open');
+		threadsGenerator.threads[0].header.should.equal('bytes=0-101');
 
 
 		//Thread 1
-		ThreadsGenerator.threads[1].start.should.equal(102);
-		ThreadsGenerator.threads[1].position.should.equal(102);
-		ThreadsGenerator.threads[1].end.should.equal(202);
-		ThreadsGenerator.threads[1].connection.should.equal('open');
-		ThreadsGenerator.threads[1].header.should.equal('bytes=102-202');
+		threadsGenerator.threads[1].start.should.equal(102);
+		threadsGenerator.threads[1].position.should.equal(102);
+		threadsGenerator.threads[1].end.should.equal(202);
+		threadsGenerator.threads[1].connection.should.equal('open');
+		threadsGenerator.threads[1].header.should.equal('bytes=102-202');
 
 		//Thread 2
-		ThreadsGenerator.threads[2].start.should.equal(203);
-		ThreadsGenerator.threads[2].position.should.equal(203);
-		ThreadsGenerator.threads[2].end.should.equal(303);
-		ThreadsGenerator.threads[2].connection.should.equal('open');
-		ThreadsGenerator.threads[2].header.should.equal('bytes=203-303');
+		threadsGenerator.threads[2].start.should.equal(203);
+		threadsGenerator.threads[2].position.should.equal(203);
+		threadsGenerator.threads[2].end.should.equal(303);
+		threadsGenerator.threads[2].connection.should.equal('open');
+		threadsGenerator.threads[2].header.should.equal('bytes=203-303');
 
 		//Thread 3
-		ThreadsGenerator.threads[3].start.should.equal(304);
-		ThreadsGenerator.threads[3].position.should.equal(304);
-		ThreadsGenerator.threads[3].end.should.equal(401);
-		ThreadsGenerator.threads[3].connection.should.equal('open');
-		ThreadsGenerator.threads[3].header.should.equal('bytes=304-401');
+		threadsGenerator.threads[3].start.should.equal(304);
+		threadsGenerator.threads[3].position.should.equal(304);
+		threadsGenerator.threads[3].end.should.equal(401);
+		threadsGenerator.threads[3].connection.should.equal('open');
+		threadsGenerator.threads[3].header.should.equal('bytes=304-401');
 
 	});
 
