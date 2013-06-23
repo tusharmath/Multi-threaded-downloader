e4f8aeebd99128baa04f5bbe3fb8e7bbc0a3d5dc
Tushar Mathur <tusharmath@gmail.com>
2013-06-23 11:07:54 +0530

    Factory.js working

--- demo/download_demo.js
+++ demo/download_demo.js
@@ -1,13 +1,31 @@
-var core = require('../lib/core');
+var mtd = require('../lib');
 
 var options = {
 	url: 'http://nodejs.org/dist/v0.10.5/node-v0.10.5.pkg',
-	checksum: 'sha1:4a7c59c37afa8ac7b6f2d1d83b34de30f24642ae'
+	checksum: 'sha1:4a7c59c37afa8ac7b6f2d1d83b34de30f24642ae',
+	fileName: '/Users/tusharmathur/downloads/node-v0.10.5.pkg',
+	threads: 32,
+	method: 'GET',
+	port: 80,
+	range: '0-100'
 };
 
 
-var downloader = new core(options);
-downloader.download();
+
+var downloader = new mtd(options);
+
+downloader.onStart = _onStart;
+downloader.onEnd = _onEnd;
+downloader.onThreadStart = _onThreadStart;
+downloader.onThreadComplete = _onThreadComplete;
+downloader.onThreadFail = _onThreadFail;
+
+downloader.start('/Users/tusharmathur/node-download.mtd');
+//OR 
+downloader.start();
+
+downloader.stop();
+downloader.restart();
 
 /*
 440 seconds video HD = 94260006
--- test/test.Factory.js
+++ test/test.Factory.js
@@ -1,5 +1,5 @@
 var should = require('should');
-var Factory = require('../lib/utils/Factory');
+var Factory = require('../lib/Factory');
 var FakeModule = require('../lib/core/FakeModule');
 var Mocked = require('./mock/mock.requires');
 var fs = require('fs');
