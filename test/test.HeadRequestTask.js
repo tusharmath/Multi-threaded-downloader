cf9bcab054de37728ca695b1a78cc900a3fe3be6
Tushar Mathur <tusharmath@gmail.com>
2013-06-25 00:54:24 +0530

    Adding Head request Task

--- test/test.HeadRequestTask.js
+++ test/test.HeadRequestTask.js
@@ -1,18 +1,30 @@
 var mockery = require('./mock/mock.requires');
 var should = require('should');
-var HeadRequestTask = require('../lib/core/HeadRequestTask');
 
-suite('HeadRequestTask', function() {
-	setup(function() {
-		mockery.enable();
+var HeadRequestTask;
+describe('HeadRequestTask', function() {
+	before(function() {
+		mockery.enable({
+			useCleanCache: true
+		});
+
+		HeadRequestTask = require('../lib/core/HeadRequestTask');
 	});
 
-	test(function() {
+	after(function() {
+		mockery.disable();
+	});
+
+	it('test execute method', function() {
 		var headRequestTask = new HeadRequestTask('http://random/dom');
+
 		var headRequestMade = false;
-		headRequestTask.callback = function() {
+
+		headRequestTask.callback = function(data) {
 			headRequestMade = true;
 		};
+
+		headRequestTask.execute();
 		headRequestMade.should.be.ok;
 	});
 });
