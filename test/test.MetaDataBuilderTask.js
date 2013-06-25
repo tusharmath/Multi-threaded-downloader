be8a4de555bf93ccc699a5e63653ec243af7a36f
Tushar Mathur <tusharmath@gmail.com>
2013-06-25 11:52:50 +0530

    Sync Tasks will have execute and callbacks

--- test/test.MetaDataBuilderTask.js
+++ test/test.MetaDataBuilderTask.js
@@ -3,18 +3,23 @@ var DataBuilder = require('../lib/core/MetaDataBuilderSyncTask');
 
 describe('MetaDataBuilderSyncTask', function() {
 	it('test new file', function() {
-		var builder = new DataBuilder({
-			file: '/Users/downloads/demo.pkg'
-		});
-		builder.file.should.equal('/Users/downloads/demo.pkg.mtd');
+		var builder = new DataBuilder('/Users/downloads/demo.pkg');
+		var response;
+		builder.callback = function(data) {
+			response = data;
+		};
+		builder.execute();
+		response.file.should.equal('/Users/downloads/demo.pkg.mtd');
 
 	});
 
 	it('test already present file file', function() {
-		var builder = new DataBuilder({
-			file: '/Users/downloads/demo.pkg.mtd'
-		});
-		builder.file.should.equal('/Users/downloads/demo.pkg.mtd');
-
+		var builder = new DataBuilder('/Users/downloads/demo.pkg.mtd');
+		var response;
+		builder.callback = function(data) {
+			response = data;
+		};
+		builder.execute();
+		response.file.should.equal('/Users/downloads/demo.pkg.mtd');
 	});
 });
--- test/test.ThreadsGeneratorSyncTask.js
+++ test/test.ThreadsGeneratorSyncTask.js
@@ -3,17 +3,22 @@ var ThreadsGenerator = require('../lib/core/ThreadsGeneratorSyncTask');
 
 describe('ThreadGeneratorTask', function() {
 	it('test creation task', function() {
-		var threads = new ThreadsGenerator(100, {
+		var generator = new ThreadsGenerator(100, {
 			count: 4
 		});
-		threads[0].start.should.equal(0);
-		threads[0].end.should.equal(25);
-		threads[1].start.should.equal(26);
-		threads[1].end.should.equal(50);
-		threads[2].start.should.equal(51);
-		threads[2].end.should.equal(75);
-		threads[3].start.should.equal(76);
-		threads[3].end.should.equal(100);
+		var _threads = [];
+		generator.callback = function(threads) {
+			_threads = threads;
+		};
+		generator.execute();
+		_threads[0].start.should.equal(0);
+		_threads[0].end.should.equal(25);
+		_threads[1].start.should.equal(26);
+		_threads[1].end.should.equal(50);
+		_threads[2].start.should.equal(51);
+		_threads[2].end.should.equal(75);
+		_threads[3].start.should.equal(76);
+		_threads[3].end.should.equal(100);
 
 	});
 });
