72b07fe3a172c7e8e89f1a8044f016108e1acdb7
Tushar Mathur <tusharmath@gmail.com>
2013-07-02 19:38:34 +0530

    updated demo files

--- demo/NewDownload.js
+++ demo/NewDownload.js
@@ -6,10 +6,16 @@ var options = {
 	port: 80,
 	range: '0-100',
 	timeout: 5000,
+	onStart: function(meta) {
+		console.log('Download Started');
+	},
+	onThreadChanged: function(threads) {
+		console.log('Data was written to the file');
+	},
 	//TODO : Implement
 	retry: true
 };
-var url = 'http://joaomoreno.github.io/thyme/dist/Thyme-0.4.2.dmg';
-var file = '/Users/tusharmathur/Desktop/temp/Thyme-0.4.2.dmg';
+var url = 'http://download-ln.jetbrains.com/teamcity/TeamCity-8.0.tar.gz';
+var file = '/Users/tusharmathur/Desktop/temp/TeamCity-8.0.tar.gz';
 var downloader = new mtd(file, url, options);
 downloader.start();
