fbe772289e695345dc9594bc2f9474a9ceabc242
Tushar Mathur <tusharmath@gmail.com>
2015-03-18 22:20:34 +0530

    fix #23 enable cookie support

--- test/test.HeadRequestTask.js
+++ test/test.HeadRequestTask.js
@@ -1,10 +1,11 @@
 var mockery = require('./mock/mock.requires');
+var sinon = require('sinon');
 var should = require('should');
 var e = require('../lib/Exceptions');
 
 var HeadRequestTask;
-describe('HeadRequestTask', function() {
-	before(function() {
+describe('HeadRequestTask', function () {
+	before(function () {
 		mockery.enable({
 			warnOnUnregistered: false
 		});
@@ -12,15 +13,19 @@ describe('HeadRequestTask', function() {
 		HeadRequestTask = require('../lib/core/HeadRequestAsyncTask');
 	});
 
-	after(function() {
+	after(function () {
 		mockery.disable();
 	});
 
-	it('test execute method', function(done) {
+	afterEach(function () {
+		mockery.resetCache();
+	});
+
+	it('test execute method', function (done) {
 
 		var req = new HeadRequestTask('http://random/dom');
 
-		var callback = function(err, data) {
+		var callback = function (err, data) {
 			if (data) {
 				data.fileSize.should.equal(100);
 				data.headers.should.equal(mockery.Fake_HttpBodyResponse.headers);
@@ -32,11 +37,11 @@ describe('HeadRequestTask', function() {
 
 	});
 
-	it('test onerror method', function(done) {
+	it('test onerror method', function (done) {
 
 		var req = new HeadRequestTask('http://random/dom');
 
-		var callback = function(err, data) {
+		var callback = function (err, data) {
 			if (err) {
 				err.should.eql(e(1004, 'random'));
 				done();
@@ -45,5 +50,32 @@ describe('HeadRequestTask', function() {
 		};
 		req.execute(callback);
 
+	});
+	describe('custom headers', function () {
+		var http;
+		beforeEach(function () {
+			http = require('http')
+			sinon.stub(http, 'request')
+				.returns({
+					on: sinon.stub()
+						.returns({
+							end: sinon.stub()
+						})
+				});
+		});
+		afterEach(function () {
+			http.request.restore();
+		});
+		it('requests with custom headers', function () {
+			var requestHeader = {};
+			var req = new HeadRequestTask('http://random/dom', {
+				headers: requestHeader
+			});
+
+			req.execute(function () {});
+			http.request.callCount.should.equal(1);
+			http.request.getCall(0)
+				.args[0].headers.should.equal(requestHeader);
+		});
 	});
 });
