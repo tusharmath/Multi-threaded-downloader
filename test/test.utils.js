ebfc6bc718f991b61a7291d5c4cff2ce9a4a63ce
tusharmath <tusharmath@gmail.com>
2015-05-15 12:41:37 +0530

    add cache support

--- test/test.utils.js
+++ test/test.utils.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 chai.use(require('chai-as-promised'));
 var should = chai.should();
 var expect = chai.expect;
-var u = require('../lib/utils');
+var U = require('../lib/utils');
 describe('utils', function () {
     function successful(a, b, c, d) {
         setTimeout(function () {
@@ -15,6 +15,10 @@ describe('utils', function () {
             d('sample-exception');
         })
     }
+    var u;
+    beforeEach(function (){
+        u = U();
+    });
 
     describe('qromise()', function () {
         it('should return promises', function () {
@@ -26,6 +30,23 @@ describe('utils', function () {
         it('should reject promises', function () {
             return u.qromise(failed)(100, 200, 300)
                 .should.be.rejectedWith('sample-exception')
+        });
+    });
+
+    describe("cache()", function () {
+        var cache;
+        beforeEach(function (){
+            cache = u.cache();
+        });
+        it("creates a getter and setter", function () {
+            cache.set('hello', 100);
+            cache.get('hello').should.equal(100);
+        });
+
+        it("throws", function () {
+            expect(function(){
+                cache.get('hello');
+            }).to.throw('no cache value set for: hello')
         });
     });
 });
