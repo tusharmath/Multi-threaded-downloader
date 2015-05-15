66ea7ad39a914cae8aca737b76b2af0f152084a5
tusharmath <tusharmath@gmail.com>
2015-05-15 20:18:25 +0530

    able to download files

--- index.js
+++ index.js
@@ -3,35 +3,108 @@
  */
 "use strict";
 var _ = require('lodash');
-var u = {};
+var request = require('request');
 var fs = require('fs');
 var event = require('common-js-pub-sub');
-
-u.stripFirstParam = function (func){
-    return function (err, res){
-        if(err instanceof Error){
+var utils = function (params, ev) {
+    var u = {
+        //DIRTY METHODS
+        onError: function (err) {
             throw err;
+        },
+
+        setProperty: function (obj, key, val) {
+            obj[key] = val;
+        },
+
+        extractAndSetProperty: function (obj, key, func, val) {
+            u.setProperty(obj, key, func(val));
+        },
+
+        getContentLength: function (res) {
+            return parseInt(res.headers['content-length'], 10);
+        },
+
+        addParamPosition: function (val) {
+            return params.position + val;
+        },
+
+        stripFirstParamAsError: function (func) {
+            return function (err, res) {
+                if (err instanceof Error) {
+                    throw err;
+                }
+                func(res);
+            };
+        },
+
+        getItemLength: function (item) {
+            return item.length;
+        },
+
+        writeBufferAtPosition: function (buffer) {
+            console.log(params.position)
+            fs.write(params.fd, buffer, 0, u.getItemLength(buffer), params.position, u.TRIGGER_DATA_SAVE);
+        },
+
+        makeRequest: function () {
+            request(params.uri, params.headers)
+                .on('data', u.TRIGGER_DATA_RECEIVE)
+                .on('response', u.TRIGGER_DATA_START)
+                .on('error', u.TRIGGER_ERROR)
+        },
+        updateAndSetPositionOnParams : function (buffer){
+            params.position += buffer.length;
         }
-        func(res);
     };
+    //EVENTS
+    u.TRIGGER_FILE_OPEN = u.stripFirstParamAsError(_.partial(ev.publish, 'FILE_OPEN'));
+    u.TRIGGER_DATA_SAVE = u.stripFirstParamAsError(_.partial(ev.publish, 'DATA_SAVE'));
+    u.TRIGGER_DATA_RECEIVE = _.partial(ev.publish, 'DATA_RECEIVE');
+    u.TRIGGER_DATA_START = _.partial(ev.publish, 'DATA_START');
+    u.TRIGGER_ERROR = _.partial(ev.publish, 'ERROR');
+    //THUNKS (PRETTY METHODS!)
+    u.createFileDescriptor = _.partial(_.ary(fs.open, 3), params.path, 'w+', u.TRIGGER_FILE_OPEN);
+    u.setFileDescriptorOnParams = _.partial(u.setProperty, params, 'fd');
+    u.extractAndSetContentLengthOnParams = _.partial(u.extractAndSetProperty, params, 'totalFileSize', u.getContentLength);
+    u.setDownloadedBytes = _.partial(u.setProperty, params, 'downloadedBytes');
+    return u;
+};
+
+var defaultOptions = {
+    headers: {}
 };
 function download(options) {
+    options = _.assign(options, defaultOptions);
     return {
         start: function (cb) {
-            var ev = event();
+            var params = {
+                position: 0, path: options.path, uri: options.uri
+            }, ev = event();
+            var u = utils(params, ev);
 
+            //Thunks
+            //TODO: Public to test
+            ev.subscribe('ERROR', u.onError);
 
-            var triggerFileOpen = _.partial(ev.publish, 'file.open');
-            var openFile = _.partial(fs.open, 'w+', triggerFileOpen);
+            ev.subscribe('INIT', u.createFileDescriptor);
 
-            ev.subscribe('init', function (val) {
-                fs.open(options.path, 'w+', triggerFileOpen);
-            });
-            ev.publish('init', {});
+            ev.subscribe('FILE_OPEN', u.makeRequest);
+            ev.subscribe('FILE_OPEN', u.setFileDescriptorOnParams);
+
+            ev.subscribe('DATA_START', u.extractAndSetContentLengthOnParams);
+
+            ev.subscribe('DATA_RECEIVE', u.writeBufferAtPosition);
+            ev.subscribe('DATA_RECEIVE', u.updateAndSetPositionOnParams);
+
+            ev.subscribe('DATA_SAVE', u.setDownloadedBytes);
+
+            //START
+            ev.publish('INIT', null);
 
         }
     }
 }
 
-exports.utils = u;
+exports.utils = utils;
 exports.download = download;
--- test/test.index.js
+++ test/test.index.js
@@ -1,25 +1,86 @@
 /**
  * Created by tusharmathur on 5/15/15.
  */
-var u = require('../').utils;
+var fs = require('fs');
+var mockery = require('mockery');
+var utils = require('../').utils;
 var sinon = require('sinon');
 var chai = require('chai');
+var event = require('common-js-pub-sub');
 var expect = chai.expect;
+
 chai.should();
 
 describe('Downloader', function () {
-    describe("stripFirstParam()", function () {
+    var u, params, fsStub, ev;
+
+    beforeEach(function () {
+        params = {
+            path: 'file.txt',
+            fd: {},
+            totalFileSize: 0,
+            position: 120
+        };
+        ev = event();
+        sinon.stub(ev, 'subscribe');
+        sinon.stub(ev, 'publish');
+        sinon.stub(fs, 'open');
+        sinon.stub(fs, 'write');
+        u = utils(params, ev);
+
+    });
+    afterEach(function () {
+        fs.open.restore();
+        fs.write.restore();
+        ev.subscribe.restore();
+        ev.publish.restore();
+    });
+
+    describe("stripFirstParamAsError()", function () {
         it("strips first param", function () {
             var temp = sinon.spy();
-            u.stripFirstParam(temp)(100, 200);
+            u.stripFirstParamAsError(temp)(100, 200);
             temp.calledWith(200).should.be.ok;
         });
 
         it("throws if type is error", function () {
             var temp = sinon.spy();
-            expect(() => u.stripFirstParam(temp)(Error(100), 200))
-            .to.throw('100');
+            expect(() => u.stripFirstParamAsError(temp)(Error(100), 200))
+                .to.throw('100');
         });
+    });
 
+    it("setFileDescriptorOnParams()", function () {
+        u.setFileDescriptorOnParams(100);
+        params.fd.should.equal(100);
+    });
+
+    it("createFileDescriptor()", function () {
+        u.createFileDescriptor(1,2,3);
+        fs.open.calledWith('file.txt', 'w+', u.TRIGGER_FILE_OPEN).should.be.ok;
+        fs.open.getCall(0).args.length.should.equal(3);
+
+    });
+
+    it("setFileDescriptorOnParams()", function () {
+        u.setFileDescriptorOnParams(1000);
+        params.fd.should.equal(1000);
+    });
+
+    it("writeBufferAtPosition()", function () {
+        var buffer = {length: 100};
+        u.writeBufferAtPosition(buffer);
+        fs.write.calledWith(params.fd, buffer, 0, 100, 120, u.TRIGGER_DATA_SAVE).should.be.ok;
+    });
+
+    it("extractAndSetContentLengthOnParams()", function () {
+        u.extractAndSetContentLengthOnParams({headers:{'content-length': 1000}});
+        params.totalFileSize.should.equal(1000);
+    });
+
+    it("updateAndSetPositionOnParams()", function () {
+        var buffer = {length: 1000};
+        u.updateAndSetPositionOnParams(buffer);
+        params.position.should.equal(1120);
     });
 });
