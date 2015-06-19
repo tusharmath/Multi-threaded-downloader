9d20fad290f5303d2b0fd0c39f00b14c89db0e34
tusharmath <tusharmath@gmail.com>
2015-06-19 13:21:07 +0530

    add integration test

---
+++ test/test.integration.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var downloader = require('../').download,
+    chai = require('chai'),
+    crypto = require('crypto'),
+    fs = require('fs'),
+    md5HashBuilder = crypto.createHash('md5'),
+    FILENAME = './out',
+    mtd = downloader({
+        path: FILENAME
+    }),
+    s = fs.ReadStream(FILENAME),
+    uri = 'http://i.ytimg.com/vi/pzPxhaYQQK8/hqdefault.jpg';
+
+describe('RealDownload', function () {
+
+    it("download pug picture", function (done) {
+        this.timeout(5000);
+        mtd.start(uri, function () {
+            s.on('data', function (d) {
+                md5HashBuilder.update(d);
+            });
+            s.on('end', function () {
+                var md5Digest = md5HashBuilder.digest('hex');
+                md5Digest.toLowerCase().should.equal('ef278f46709ebb6f6d84cefc57c0caef');
+                done();
+            });
+        });
+    });
+});
+
