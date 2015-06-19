2695b9feefae669052af5845efbd3fbd601208cf
tusharmath <tusharmath@gmail.com>
2015-06-19 15:56:31 +0530

    rename variables

--- test/test.new-download.js
+++ test/test.new-download.js
@@ -9,18 +9,18 @@ var downloader = require('../').download,
     mtd = downloader({
         path: FILENAME
     }),
-    s = fs.ReadStream(FILENAME),
+    fileStream = fs.ReadStream(FILENAME),
     uri = 'http://i.ytimg.com/vi/pzPxhaYQQK8/hqdefault.jpg';
 chai.should();
-describe('RealDownload', function () {
+describe('NewDownload', function () {
 
     it("download pug picture", function (done) {
         this.timeout(5000);
         mtd.start(uri, function () {
-            s.on('data', function (d) {
+            fileStream.on('data', function (d) {
                 md5HashBuilder.update(d);
             });
-            s.on('end', function () {
+            fileStream.on('end', function () {
                 var md5Digest = md5HashBuilder.digest('hex');
                 md5Digest.toLowerCase().should.equal('ef278f46709ebb6f6d84cefc57c0caef');
                 done();
