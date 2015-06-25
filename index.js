2e0467d5db94b40cf05d3551c994e34d39a55b3f
tusharmath <tusharmath@gmail.com>
2015-06-25 09:41:58 +0530

    improve code quality

--- index.js
+++ index.js
@@ -4,160 +4,87 @@
 "use strict";
 var _ = require('lodash');
 var request = require('request');
+var co = require('co');
 var fs = require('fs');
-var event = require('common-js-pub-sub');
-var MAX_BUFFER = 1000;
-var utils = function (params, ev) {
-    var u = {
-        //DIRTY METHODS
-        onError: function (err) {
-            if (err instanceof Error) {
-                throw err;
-            } else {
-                throw Error(err);
-            }
-        },
-
-        setProperty: function (obj, key, val) {
-            if (!key) {
-                _.assign(obj, val);
-            } else {
-                obj[key] = val;
-            }
-        },
-
-        extractAndSetProperty: function (obj, key, func, val) {
-            u.setProperty(obj, key, func(val));
-        },
-
-        getContentLength: function (res) {
-            return parseInt(res.headers['content-length'], 10);
-        },
-        getFileSize: function (stats) {
-            return parseInt(stats.size, 10);
-        },
-
-        addParamPosition: function (val) {
-            return params.position + val;
-        },
-
-        stripFirstParamAsError: function (func) {
-            return function (err, res) {
-                if (err instanceof Error) {
-                    u.onError(err);
-                }
-                func(res);
-            };
-        },
-
-        getItemLength: function (item) {
-            return item.length;
-        },
-
-        writeBufferAtPosition: function (buffer) {
-            fs.write(params.fd, buffer, 0, u.getItemLength(buffer), params.position, u.DATA_SAVE);
-        },
-
-        makeRequest: function () {
-            request(params.uri, params.headers)
-                .on('data', u.DATA_RECEIVE)
-                .on('response', u.DATA_START)
-                .on('error', u.ERROR)
-                .on('complete', u.DATA_COMPLETE)
-        },
-        updateAndSetPositionOnParams: function (buffer) {
-            params.position += buffer.length;
-        },
-        createTriggerFor: function (eventName) {
-            return _.partial(ev.publish, eventName);
-        },
-        saveDownloadedBytes: function () {
-        saveParamsAsMeta: function () {
-            var buf = new Buffer(MAX_BUFFER);
-            _.fill(buf, null);
-            buf.write(JSON.stringify(params));
-            fs.write(params.fd, buf, 0, buf.length, params.totalFileSize, u.METADATA_SAVE);
-        },
-        truncate: function () {
-            fs.truncate(params.fd, params.totalFileSize, u.FILE_TRUNCATE);
-        },
-        rename: function () {
-            fs.rename(params.path, params.path.replace('.mtd', ''), u.FILE_RENAME);
-        },
-        checkForCompletion: function () {
-            if (params.position === params.totalFileSize) {
-                u.FILE_COMPLETE();
-            }
-        }
-
-    };
-    //THUNKS (PRETTY METHODS!)
-    u.stripErrorParamAndCreateTrigger = _.flow(u.createTriggerFor, u.stripFirstParamAsError);
-
-    //EVENTS
-    //File:DATA
-    u.FILE_OPEN = u.stripErrorParamAndCreateTrigger('FILE_OPEN');
-    u.FILE_TRUNCATE = u.stripErrorParamAndCreateTrigger('FILE_TRUNCATE');
-    u.FILE_RENAME = u.stripErrorParamAndCreateTrigger('FILE_RENAME');
-    u.FILE_COMPLETE = _.once(u.createTriggerFor('FILE_COMPLETE'));
-
-    //FILE:META
-    u.METADATA_SAVE = u.stripErrorParamAndCreateTrigger('METADATA_SAVE');
-
-    //HTTP:DATA
-    u.DATA_RECEIVE = u.createTriggerFor('DATA_RECEIVE');
-    u.DATA_COMPLETE = u.createTriggerFor('DATA_COMPLETE');
-    u.DATA_START = u.createTriggerFor('DATA_START');
-    u.DATA_SAVE = u.stripErrorParamAndCreateTrigger('DATA_SAVE');
-
-    u.ERROR = _.partial(ev.publish, 'ERROR');
-
-    u.createFileDescriptor = _.partial(_.ary(fs.open, 3), params.path, 'w+', u.FILE_OPEN);
-    u.setFileDescriptorOnParams = _.partial(u.setProperty, params, 'fd');
-    u.extractAndSetContentLengthOnParams = _.partial(u.extractAndSetProperty, params, 'totalFileSize', u.getContentLength);
-    u.setDownloadedBytesOnParams = _.partial(u.setProperty, params, 'downloadedBytes');
-
-    return u;
-};
+var MAX_BUFFER = 512;
 
 var defaultOptions = {
     headers: {}
 };
 function download(options) {
     options = _.assign(options, defaultOptions);
+    options.path += '.mtd';
     return {
-        start: function (uri, cb) {
-            var params = {
-                position: 0, path: options.path + '.mtd', uri: uri
-            }, ev = event();
-            var u = utils(params, ev);
+        start: co.wrap(function * (uri) {
+            var position = 0, totalBytes, fd, defer, dataComplete = false;
 
-            ev.subscribe('FILE_RENAME', cb);
-            ev.subscribe('FILE_TRUNCATE', u.rename);
-            ev.subscribe('FILE_COMPLETE', u.truncate);
-            ev.subscribe('ERROR', u.onError);
+            var u = {
+                async: function (func) {
+                    var coFunc = co.wrap(func);
+                    return function () {
+                        try {
+                            coFunc.apply(null, arguments).catch(u.onError);
+                        } catch (e) {
+                            u.onError(e);
+                        }
+                    };
+                },
+                onError: function (err) {
+                    defer.reject(err);
+                },
+                promisify: function (func) {
+                    var defer = Promise.defer(),
+                        handle = function (err, data) {
+                            if (err) {
+                                defer.reject(err);
+                            } else {
+                                defer.resolve(data);
+                            }
+                        };
+                    return _.restParam(function (args) {
+                        args.push(handle);
+                        func.apply(null, args);
+                        return defer.promise;
+                    })
+                }
+            };
 
-            ev.subscribe('INIT', u.createFileDescriptor);
+            u.fsWrite = u.promisify(fs.write);
+            u.fsOpen = u.promisify(fs.open);
+            u.fsTruncate = u.promisify(fs.truncate);
+            u.fsRename = u.promisify(fs.rename);
+            fd = yield u.fsOpen(options.path, 'w+');
+            defer = Promise.defer();
+            var onData = function *(buffer) {
+                var writeAt = position;
+                position += buffer.length;
+                var written = yield u.fsWrite(fd, buffer, 0, buffer.length, writeAt);
+                var meta = new Buffer(MAX_BUFFER);
+                _.fill(meta, null);
+                meta.write(JSON.stringify({position, totalBytes, uri}));
+                yield u.fsWrite(fd, meta, 0, meta.length, totalBytes);
 
-            ev.subscribe('FILE_OPEN', u.makeRequest);
-            ev.subscribe('FILE_OPEN', u.setFileDescriptorOnParams);
+                if (dataComplete) {
+                    yield u.fsTruncate(fd, totalBytes);
+                    yield u.fsRename(options.path, options.path.replace('.mtd', ''));
+                    defer.resolve();
+                }
+            };
+            var onResponse = function * (res) {
+                return totalBytes = parseInt(res.headers['content-length'], 10);
+            };
+            var onComplete = function * () {
+                dataComplete = true;
+            };
 
-            ev.subscribe('DATA_START', u.extractAndSetContentLengthOnParams);
+            request(uri)
+                .on('data', u.async(onData))
+                .on('response', u.async(onResponse))
+                .on('complete', u.async(onComplete));
 
-            ev.subscribe('DATA_RECEIVE', u.writeBufferAtPosition);
-            ev.subscribe('DATA_RECEIVE', u.updateAndSetPositionOnParams);
-
-            ev.subscribe('DATA_SAVE', u.setDownloadedBytesOnParams);
-            ev.subscribe('DATA_SAVE', u.saveParamsAsMeta);
-
-            ev.subscribe('METADATA_SAVE', u.checkForCompletion);
-
-            //START
-            ev.publish('INIT', null);
-
-        }
+            return yield defer.promise;
+        })
     }
 }
 
-exports.utils = utils;
 exports.download = download;
--- test/test.new-download.js
+++ test/test.new-download.js
@@ -12,20 +12,24 @@ var downloader = require('../').download,
     fileStream = fs.ReadStream(FILENAME),
     uri = 'http://i.ytimg.com/vi/pzPxhaYQQK8/hqdefault.jpg';
 chai.should();
+chai.use(require("chai-as-promised"));
 describe('NewDownload', function () {
 
-    it("download pug picture", function (done) {
+    it("download pug picture", function () {
         this.timeout(5000);
-        mtd.start(uri, function () {
-            fileStream.on('data', function (d) {
-                md5HashBuilder.update(d);
+        return mtd.start(uri).then(function () {
+            var defer = Promise.defer();
+            var shasum = crypto.createHash('sha1');
+            var s = fs.ReadStream(FILENAME);
+            s.on('data', function (d) {
+                shasum.update(d);
             });
-            fileStream.on('end', function () {
-                var md5Digest = md5HashBuilder.digest('hex');
-                md5Digest.toLowerCase().should.equal('ef278f46709ebb6f6d84cefc57c0caef');
-                done();
+
+            s.on('end', function () {
+                defer.resolve(shasum.digest('hex'))
             });
-        });
+            return defer.promise;
+        }).should.eventually.equal('c4ad77ad6468c54b11bcbc52cb56cb938c6a2dee');
     });
 });
 
