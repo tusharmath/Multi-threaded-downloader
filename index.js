c5183df71900f8c4bd8cb21bf8e08a7714781a17
tusharmath <tusharmath@gmail.com>
2015-06-25 18:15:51 +0530

    added function for creating meta data

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ var _ = require('lodash'),
 var defaultOptions = {
     headers: {}
 };
+var createMetaData = function (totalBytes, url, path, count) {
+    var threads = _.fill(_.times(count), {});
+    var data = {totalBytes, url, path, threads};
+    return {
+        toBuffer: function () {
+            var metaBuffer = new Buffer(MAX_BUFFER);
+            _.fill(metaBuffer, null);
+            metaBuffer.write(JSON.stringify(data));
+            return metaBuffer;
+        },
+        updatePosition: function (index, distance) {
+            data.threads[index].position += distance;
+        },
+        setRange: function (index, range) {
+            var thread = data.threads[index];
+            thread.start = range.start;
+            thread.end = range.end;
+            thread.position = range.start;
+        }
+    }
+};
 function download(options) {
     options = _.assign(options, defaultOptions);
     options.path += '.mtd';
@@ -18,6 +39,7 @@ function download(options) {
         start: co.wrap(function * (url) {
             var dataComplete = false,
                 totalBytes = parseInt((yield u.requestHead(url)).headers['content-length'], 10),
+                meta = createMetaData(totalBytes, url, options.path, THREAD_COUNT),
                 fd = yield u.fsOpen(options.path, 'w+');
             var iterable = _.times(THREAD_COUNT, function (threadIndex) {
                 var defer = Promise.defer(),
@@ -26,18 +48,19 @@ function download(options) {
                     position = Math.floor(bytesPerThread * threadIndex),
                     endPosition = THREAD_COUNT - threadIndex === 1 ? totalBytes : position + bytesPerThread - 1,
                     headers = {'range': `bytes=${position}-${endPosition}`};
-
+                meta.setRange(threadIndex, position, endPosition);
                 request({url, headers})
                     .on('data', async(function *(buffer) {
 
                         //Write Data
                         position += buffer.length;
+
+                        //console.log(position, headers, buffer.length);
                         yield u.fsWrite(fd, buffer, 0, buffer.length, position - buffer.length);
+                        meta.updatePosition(threadIndex, buffer.length);
 
                         //Write Meta
-                        var metaBuffer = new Buffer(MAX_BUFFER);
-                        _.fill(metaBuffer, null);
-                        metaBuffer.write(JSON.stringify({position, totalBytes, url}));
+                        var metaBuffer = meta.toBuffer();
                         yield u.fsWrite(fd, metaBuffer, 0, metaBuffer.length, totalBytes);
 
                         //Data Completed
