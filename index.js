ce75b268041b30f0dd0e78f7a2c85640665dbca5
tusharmath <tusharmath@gmail.com>
2015-06-26 16:20:44 +0530

    Refactor

--- index.js
+++ index.js
@@ -2,19 +2,88 @@
  * Created by tusharmathur on 5/15/15.
  */
 "use strict";
-var _ = require('lodash'),
+var fs = require('fs'),
+    _ = require('lodash'),
     request = require('request'),
-    co = require('co'),
-    THREAD_COUNT = 2,
-    u = require('./Utility');
-var defaultOptions = {
-    headers: {}
-}, writeBufferAt;
+    co = require('co');
 
-var connectionCompleted = function (connection, thread) {
-    if (connection.complete && thread.hasCompleted()) {
-        connection.resolve();
-    }
+var defaultOptions = {
+    headers: {},
+    threadCount: 3
+};
+var fsWrite = promisify(fs.write),
+    fsOpen = promisify(fs.open),
+    fsTruncate = promisify(fs.truncate),
+    fsRename = promisify(fs.rename),
+    requestHead = promisify(request.head);
+
+function asyncFunc(onError, func) {
+    var coFunc = co.wrap(func);
+    return () => {
+        try {
+            coFunc.apply(null, arguments).catch(onError);
+        } catch (e) {
+            onError(e);
+        }
+    };
+}
+function promisify(func) {
+    var defer = Promise.defer(),
+        handle = (err, data) => err ? defer.reject(err) : defer.resolve(data);
+    return _.restParam(function (args) {
+        args.push(handle);
+        func.apply(null, args);
+        return defer.promise;
+    })
+}
+
+var writeData = (fd, buffer, position) => fsWrite(fd, buffer, 0, buffer.length, position);
+var getLength = (res) => parseInt(res.headers['content-length'], 10);
+var createFileDescriptor = (path) => fsOpen(path, 'w+');
+var createRangeHeader = function (count, totalBytes, i) {
+    var range = getThreadRange(count, totalBytes, i);
+    return {'range': `bytes=${range.start}-${range.end}`};
+};
+var createThread = function (range) {
+    return {
+        defer: Promise.defer(),
+        start: range.start,
+        end: range.end,
+        position: range.start,
+        updatePosition: function (distance) {
+            this.position += distance;
+            if (this.position >= this.end) {
+                this.defer.resolve(this);
+            }
+        }
+    };
+};
+function toBuffer(data, MAX_BUFFER) {
+    MAX_BUFFER = MAX_BUFFER || 512;
+    var metaBuffer = new Buffer(MAX_BUFFER);
+    _.fill(metaBuffer, null);
+    metaBuffer.write(JSON.stringify(data));
+    return metaBuffer;
+}
+function createMetaData(totalBytes, url, path, count) {
+    var MAX_BUFFER = 512;
+    var threads = _.map(_.times(count, _.partial(getThreadRange, count, totalBytes)), createThread);
+    return {totalBytes, url, path, threads};
+}
+function getThreadRange(count, total, index) {
+    var bytesPerThread = Math.round(total / count),
+        start = Math.floor(bytesPerThread * index),
+        end = count - index === 1 ? total : start + bytesPerThread - 1;
+    return {start, end}
+}
+
+var httpRequest = function (params, onData) {
+    var defer = Promise.defer();
+    request(params)
+        .on('data', asyncFunc(defer.reject, onData))
+        .on('complete', defer.resolve)
+        .on('error', defer.reject);
+    return defer.promise;
 };
 
 function download(options) {
@@ -22,40 +91,34 @@ function download(options) {
     options.path += '.mtd';
     return {
         start: co.wrap(function * (url) {
-            var totalBytes = u.getTotalBytesFromResponse(yield u.requestHead(url)),
-                meta = u.createMetaData(totalBytes, url, options.path, THREAD_COUNT),
-                fd = yield u.createFileDescriptor(options);
-            writeBufferAt = _.partial(u.writeData, fd);
-            var iterable = _.times(THREAD_COUNT, function (threadIndex) {
-                var connection = Promise.defer(),
+            var size = getLength(yield requestHead(url)),
+                meta = createMetaData(size, url, options.path, defaultOptions.threadCount),
 
-                    async = _.partial(u.async, connection.reject),
-                    range = u.getThreadRange(THREAD_COUNT, threadIndex, totalBytes),
-                    position = range.start,
-                    headers = {'range': `bytes=${range.start}-${range.end}`};
-                connection.complete = false;
-                meta.thread(threadIndex).setRange(range);
+                fd = yield createFileDescriptor(options.path),
+                _fsWrite = _.partial(writeData, fd),
+                _fsTruncate = _.partial(fsTruncate, fd),
+                rangeHeader = _.partial(createRangeHeader, defaultOptions.threadCount, size);
 
-                request({url, headers})
-                    .on('data', async(function *(buffer) {
-                        var writePosition = position;
-                        position+= buffer.length;
-                        yield writeBufferAt(buffer, writePosition);
-                        meta.thread(threadIndex).updatePosition(buffer.length);
+            var temp = function (i) {
+                var thread = meta.threads[i],
+                    position = thread.start;
+                var onData = function * (buffer) {
+                    var writePosition = position;
+                    position += buffer.length;
+                    yield _fsWrite(buffer, writePosition);
+                    thread.updatePosition(buffer.length);
+                    yield _fsWrite(toBuffer(meta), size + 1);
+                };
 
-                        yield writeBufferAt(meta.toBuffer(), totalBytes + 1);
-                        connectionCompleted(connection, meta.thread(threadIndex));
-                    }))
-                    .on('complete', async(function * () {
-                        connection.complete = true;
-                    }))
-                    .on('error', connection.reject);
-
-                return connection.promise;
-            });
+                return Promise.all([
+                    httpRequest({url, headers: rangeHeader(i)}, onData),
+                    thread.defer.promise
+                ])
+            };
+            var iterable = _.times(defaultOptions.threadCount, temp);
             return yield Promise.all(iterable).then(co.wrap(function * () {
-                yield u.fsTruncate(fd, totalBytes);
-                yield u.fsRename(options.path, options.path.replace('.mtd', ''));
+                yield _fsTruncate(size);
+                yield fsRename(options.path, options.path.replace('.mtd', ''));
             }));
         })
     }
