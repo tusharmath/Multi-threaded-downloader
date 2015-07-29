e4188c8c322ccdb2ece01e01a68c68f673475e3d
tusharmath <tusharmath@gmail.com>
2015-07-29 16:19:55 +0530

    test server character updates

--- src/perf/TestServer.js
+++ src/perf/TestServer.js
@@ -1,24 +1,32 @@
 "use strict";
 var express = require('express'),
     app = express(),
-    CHAR = '*',
     port = 3000;
 
-var defer = Promise.defer();
+var defer = Promise.defer(),
+    getChar = function* () {
+        var char = 0;
+        while (true) {
+            yield char;
+            char = char === 9 ? 0 : char + 1;
+        }
+    };
 app.get('/chunk/:size.txt', function (req, res) {
     var count = 0,
-        size = parseInt(req.params.size);
+        size = parseInt(req.params.size),
+        char = getChar();
     while (count < size) {
-        res.write(CHAR);
+        res.write(char.next().value.toString());
         count++;
     }
     res.send();
 }).get('/range/:size.txt', function (req, res) {
     var count = 0,
         str = '',
-        size = parseInt(req.params.size);
+        size = parseInt(req.params.size),
+        char = getChar();
     while (count < size) {
-        str += CHAR;
+        str += char.next().value.toString();
         count++;
     }
     res.send(str);
