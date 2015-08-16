68d3f011cb0de1f6a5a857dd0dfc49e39a802861
tushar mathur <tusharmath@gmail.com>
2015-08-16 14:42:19 +0530

    support for https via head

--- test/test.Observables.js
+++ test/test.Observables.js
@@ -13,6 +13,12 @@ describe("Observables", function () {
         response.headers['content-length'].should.equal('317235');
     });
 
+    it("HTTPS HEAD request to jpg file", function * () {
+        var url = 'https://localhost:3001/files/pug.jpg', strictSSL = false;
+        var response = yield Observables.requestHead({url, strictSSL}).toPromise();
+        response.headers['content-length'].should.equal('317235');
+    });
+
     it("HEAD request to 1024 bytes file", function * () {
         var response = yield Observables.requestHead('http://localhost:3000/range/1024.txt').toPromise();
         response.headers['content-length'].should.equal('1024');
