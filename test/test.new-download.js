c53ee65b5635aa394d05c1c97715ec401656df10
tushar mathur <tusharmath@gmail.com>
2015-08-16 13:06:41 +0530

    remove file after test completes

--- test/test.new-download.js
+++ test/test.new-download.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var Download = require('../src/app'),
+    Rx = require('Rx'),
     chai = require('chai'),
     crypto = require('crypto'),
     fs = require('fs'),
@@ -20,15 +21,28 @@ function * createDownload(url, path) {
     return yield defer.promise;
 }
 
-describe('NewDownload', function () {
-    it("download dynamically created 1024 bytes file", function * () {
-        var digest = yield createDownload('http://localhost:3000/range/1024.txt', './.temp/1024.txt');
-        digest.should.equal('41BE89713FA15BC83D093DD67E558BADA8546388'.toLowerCase());
-    });
+var removeFile = (x) => Rx.Observable.fromCallback(fs.unlink)(x).toPromise();
 
-    it("download static pug image", function * () {
-        var digest = yield createDownload('http://localhost:3000/files/pug.jpg', './.temp/pug.jpg');
-        digest.should.equal('25FD4542D7FFFB3AEC9EF0D25A533DDE4803B9C1'.toLowerCase());
+describe('NewDownload', function () {
+    describe("range-file", function () {
+        afterEach(function *() {
+            return yield removeFile('./.temp/1024.txt');
+        });
+
+        it("download dynamically created 1024 bytes file", function * () {
+            var digest = yield createDownload('http://localhost:3000/range/1024.txt', './.temp/1024.txt');
+            digest.should.equal('41BE89713FA15BC83D093DD67E558BADA8546388'.toLowerCase());
+        });
+    });
+    describe("static-file", function () {
+        afterEach(function *() {
+            return yield removeFile('./.temp/pug.jpg');
+        });
+
+        it("download static pug image", function * () {
+            var digest = yield createDownload('http://localhost:3000/files/pug.jpg', './.temp/pug.jpg');
+            digest.should.equal('25FD4542D7FFFB3AEC9EF0D25A533DDE4803B9C1'.toLowerCase());
+        });
     });
 });
 
