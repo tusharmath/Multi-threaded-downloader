c96d07a3dd8d2b444e7baabbc2219eeca5188fdb
tushar mathur <tusharmath@gmail.com>
2015-08-16 13:55:30 +0530

    refactor utility

--- src/lib/Utility.js
+++ src/lib/Utility.js
@@ -2,19 +2,23 @@
  * Created by tusharmathur on 7/14/15.
  */
 "use strict";
-var _ = require('lodash');
-exports.toBuffer = function (obj, size) {
+var _ = require('lodash'),
+    utility = {};
+
+utility.toBuffer = function (obj, size) {
     var buffer = new Buffer(size);
     _.fill(buffer, null);
     buffer.write(JSON.stringify(obj));
     return buffer;
 };
 
-exports.sliceRange = function (count, total) {
+utility.sliceRange = function (count, total) {
     var bytesPerThread = _.round(total / count);
     return _.times(count, function (index) {
         var start = bytesPerThread * index,
             end = count - index === 1 ? total : start + bytesPerThread - 1;
-        return {start, end}
+        return {start, end};
     });
 };
+
+module.exports = utility;
