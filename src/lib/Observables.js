c6509afc0e4085375e3e0754450a4aa0fded422c
tushar mathur <tusharmath@gmail.com>
2015-09-29 00:10:50 +0530

    remove unused immutable function

--- src/lib/Observables.js
+++ src/lib/Observables.js
@@ -18,13 +18,6 @@ var fsOpen = Rx.Observable.fromNodeCallback(fs.open)
 var fsWrite = Rx.Observable.fromNodeCallback(fs.write)
 var fsTruncate = Rx.Observable.fromNodeCallback(fs.truncate)
 var fsRename = Rx.Observable.fromNodeCallback(fs.rename)
-var immutable = function (obj) {
-  var observer
-  const stream = Rx.Observable.create(function (_observer) {
-    observer = _observer
-  })
-  return {stream, update: (cb) => observer.onNext(obj = cb(obj))}
-}
 module.exports = {
   requestBody,
   requestHead,
