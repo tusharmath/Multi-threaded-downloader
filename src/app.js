17038e37cf370553d246681089002ec3349db3d7
tushar mathur <tusharmath@gmail.com>
2015-09-28 19:02:34 +0530

    fix lint errors

--- src/app.js
+++ src/app.js
@@ -2,50 +2,50 @@
  * Created by tusharmathur on 5/15/15.
  */
 'use strict'
-var _ = require('lodash'),
-  utils = require('./lib/Utility'),
-  Rx = require('rx'),
-  ob = require('./lib/Observables'),
-  MAX_BUFFER = 512
+var _ = require('lodash')
+var utils = require('./lib/Utility')
+var Rx = require('rx')
+var ob = require('./lib/Observables')
+var MAX_BUFFER = 512
 
 var defaultOptions = {
   headers: {},
   threadCount: 3,
   strictSSL: true
 }
-var getContentLength = (res) =>parseInt(res.headers['content-length'], 10),
-  rangeHeader = (thread) => ({'range': `bytes=${thread.start}-${thread.end}`}),
-  toBuffer = _.partialRight(utils.toBuffer, MAX_BUFFER),
-  metaCreate = function (url, path, _ranges) {
-    var positions = _.pluck(_ranges, 'start')
-    return {url, path: path, nextByte: _.clone(positions), positions}
-  }
+var getContentLength = (res) => parseInt(res.headers['content-length'], 10)
+var rangeHeader = (thread) => ({'range': `bytes=${thread.start}-${thread.end}`})
+var toBuffer = _.partialRight(utils.toBuffer, MAX_BUFFER)
+var metaCreate = function (url, path, _ranges) {
+  var positions = _.pluck(_ranges, 'start')
+  return {url, path: path, nextByte: _.clone(positions), positions}
+}
 function * download (options) {
-  var url = options.url,
-    threadCount = options.threadCount,
-    path = options.path,
-    strictSSL = options.strictSSL,
-    size = getContentLength(yield ob.requestHead({url, strictSSL})),
-    fd = yield ob.fsOpen(path, 'w+'),
-    _meta = metaCreate(url, path, utils.sliceRange(threadCount, size)),
-    _httpRequestThread = function (range, thread) {
-      return ob.requestBody({url, strictSSL, headers: rangeHeader(range)})
-        .map(packet => _.merge(packet, {thread}))
-    },
-    _attachPacketPosition = function (packet) {
-      packet.position = _meta.nextByte[packet.thread]
-      _updateNextByte(packet)
-      return packet
-    },
-    _updateNextByte = function (packet) {
-      _meta.nextByte[packet.thread] += packet.buffer.length
-    },
-    _updatePosition = function (packet) {
-      _meta.positions[packet.thread] += packet.buffer.length
-    },
-    _fsWrite = function (fd, buffer, position) {
-      return ob.fsWrite(fd, buffer, 0, buffer.length, position)
-    }
+  var url = options.url
+  var threadCount = options.threadCount
+  var path = options.path
+  var strictSSL = options.strictSSL
+  var size = getContentLength(yield ob.requestHead({url, strictSSL}))
+  var fd = yield ob.fsOpen(path, 'w+')
+  var _meta = metaCreate(url, path, utils.sliceRange(threadCount, size))
+  var _httpRequestThread = function (range, thread) {
+    return ob.requestBody({url, strictSSL, headers: rangeHeader(range)})
+      .map(packet => _.merge(packet, {thread}))
+  }
+  var _attachPacketPosition = function (packet) {
+    packet.position = _meta.nextByte[packet.thread]
+    _updateNextByte(packet)
+    return packet
+  }
+  var _updateNextByte = function (packet) {
+    _meta.nextByte[packet.thread] += packet.buffer.length
+  }
+  var _updatePosition = function (packet) {
+    _meta.positions[packet.thread] += packet.buffer.length
+  }
+  var _fsWrite = function (fd, buffer, position) {
+    return ob.fsWrite(fd, buffer, 0, buffer.length, position)
+  }
 
   yield Rx.Observable.from(utils.sliceRange(threadCount, size))
     .selectMany(_httpRequestThread)
@@ -54,8 +54,8 @@ function * download (options) {
     .select(_updatePosition)
     .selectMany(() => _fsWrite(fd, toBuffer(_meta), size))
     .last()
-    .selectMany(()=> ob.fsTruncate(fd, size))
-    .selectMany(()=> ob.fsRename(path, path.replace('.mtd', '')))
+    .selectMany(() => ob.fsTruncate(fd, size))
+    .selectMany(() => ob.fsRename(path, path.replace('.mtd', '')))
 
   return _meta
 }
--- src/lib/Observables.js
+++ src/lib/Observables.js
@@ -1,24 +1,23 @@
-'use strict'
-var request = require('request'),
-  Rx = require('rx'),
-  _ = require('lodash'),
-  fs = require('fs')
+var request = require('request')
+var Rx = require('rx')
+var _ = require('lodash')
+var fs = require('fs')
 
 var requestBody = function (params) {
-    var responseHeaders
-    return Rx.Observable.create(function (observer) {
-      request(params)
-        .on('data', buffer => observer.onNext({buffer, headers: responseHeaders}))
-        .on('response', x => responseHeaders = x.headers)
-        .on('complete', x => observer.onCompleted(x))
-        .on('error', x => observer.onError(x))
-    })
-  },
-  requestHead = Rx.Observable.fromNodeCallback(request.head, null, _.identity),
-  fsOpen = Rx.Observable.fromNodeCallback(fs.open),
-  fsWrite = Rx.Observable.fromNodeCallback(fs.write),
-  fsTruncate = Rx.Observable.fromNodeCallback(fs.truncate),
-  fsRename = Rx.Observable.fromNodeCallback(fs.rename)
+  var responseHeaders
+  return Rx.Observable.create(function (observer) {
+    request(params)
+      .on('data', buffer => observer.onNext({buffer, headers: responseHeaders}))
+      .on('response', x => responseHeaders = x.headers)
+      .on('complete', x => observer.onCompleted(x))
+      .on('error', x => observer.onError(x))
+  })
+}
+var requestHead = Rx.Observable.fromNodeCallback(request.head, null, _.identity)
+var fsOpen = Rx.Observable.fromNodeCallback(fs.open)
+var fsWrite = Rx.Observable.fromNodeCallback(fs.write)
+var fsTruncate = Rx.Observable.fromNodeCallback(fs.truncate)
+var fsRename = Rx.Observable.fromNodeCallback(fs.rename)
 
 module.exports = {
   requestBody,
