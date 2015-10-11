f8b5e6d5d79005849e87e1bf1aa8980f232f3063
tushar mathur <tusharmath@gmail.com>
2015-10-11 10:23:10 +0530

    customise actions as needed

--- src/lib/Actions.js
+++ src/lib/Actions.js
@@ -9,7 +9,8 @@
  */
 
 const {BehaviorSubject} = require('rx')
-const Immutable = require('immutable')
+const {fromJS, Map} = require('immutable')
+const {ary} = require('lodash')
 
 class Action {
   dispatch (params) {
@@ -28,21 +29,42 @@ const setIn = x => x.store.setIn(x.path, x.value)
 const getIn = x => x.store.getIn(x.path)
 const createAction = () => new Action()
 const withStore = (action) => paramsStream(action).withLatestFrom(storeStream, selector)
+const oOrOnNext = x => storeStream.onNext(x)
 
-export const INIT = createAction()
-export const UPDATE_STORE = createAction()
-export const STORE_UPDATED = createAction()
-export const TOGGLE_STORE = createAction()
-export const INCREMENT_STORE = createAction()
-export const DECREMENT_STORE = createAction()
+// Standard
+const INIT = createAction()
+const UPDATE_STORE = createAction()
+const INIT_STORE = createAction()
+const STORE_UPDATED = createAction()
+const TOGGLE_STORE = createAction()
+const INCREMENT_STORE = createAction()
+const DECREMENT_STORE = createAction()
 
-export const actionStream = new BehaviorSubject({action: INIT, params: {}})
-export const storeStream = new BehaviorSubject(Immutable.Map({}))
-export const paramsStream = (action) => actionStream.filter(x => x.action === action).pluck('params')
+// Custom
+const DATA_RCVD = createAction()
+const DATA_SAVED = createAction()
+
+const actionStream = new BehaviorSubject({action: INIT, params: {}})
+const storeStream = new BehaviorSubject(Map({}))
+const paramsStream = (action) => actionStream.filter(x => x.action === action).pluck('params')
 
 // Global ACTION Subscribers
-storeStream.subscribe(store => STORE_UPDATED.dispatch({store}))
-withStore(UPDATE_STORE).map(setIn).distinctUntilChanged().subscribe(x => storeStream.onNext(x))
+storeStream.subscribe(store => STORE_UPDATED.dispatch(store))
+
+paramsStream(INIT_STORE).map(fromJS).subscribe(oOrOnNext)
+withStore(UPDATE_STORE).map(setIn).distinctUntilChanged().subscribe(oOrOnNext)
 withStore(TOGGLE_STORE).subscribe(x => updateStore(x.path, !getIn(x)))
 withStore(INCREMENT_STORE).subscribe(x => updateStore(x.path, getIn(x) + 1))
 withStore(DECREMENT_STORE).subscribe(x => updateStore(x.path, getIn(x) - 1))
+
+module.exports = {
+  INIT,
+  UPDATE_STORE,
+  INIT_STORE,
+  STORE_UPDATED,
+  TOGGLE_STORE,
+  INCREMENT_STORE,
+  DECREMENT_STORE,
+  DATA_RCVD,
+  DATA_SAVED
+}
