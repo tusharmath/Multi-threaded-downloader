5f5d5acddc7b6cf4feb5dcf1bd1d0786460ad003
tushar mathur <tusharmath@gmail.com>
2016-01-22 20:01:50 +0530

    refactor(test): move createFileDigest to a different function

--- perf/utils.js
+++ perf/utils.js
@@ -11,13 +11,17 @@ const Rx = require('rx')
 
 const getPath = x => Path.normalize(Path.join(__dirname, x))
 exports.removeFile = (x) => Rx.Observable.fromCallback(fs.unlink)(getPath(x)).toPromise()
+const createFileDigest = exports.createFileDigest = path => {
+  const hash = crypto.createHash('sha1')
+  return new Promise(resolve => fs
+    .createReadStream(path)
+    .on('data', x => hash.update(x))
+    .on('end', () => resolve(hash.digest('hex')))
+  )
+}
 exports.createDownload = function (parameters) {
   var url = parameters.url
   var path = getPath(parameters.path)
   var mtd = new Download({path, url, strictSSL: false})
-  var hash = crypto.createHash('sha1')
-  return mtd.start().then(() => new Promise(resolve => fs.createReadStream(path)
-    .on('data', x => hash.update(x))
-    .on('end', () => resolve(hash.digest('hex')))
-  ))
+  return mtd.start().then(() => createFileDigest(path))
 }
