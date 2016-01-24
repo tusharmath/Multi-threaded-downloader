35d3c371930ebd8802ecd8bf7f611f575d60a9f3
tushar mathur <tusharmath@gmail.com>
2016-01-24 13:57:08 +0530

    feat(create): return the meta information post creation

--- src/create.js
+++ src/create.js
@@ -8,14 +8,14 @@ const Rx = require('rx')
 const _ = require('lodash')
 const u = require('./utils')
 
+// TODO: Add unit tests
 exports.create = (ob, options) => {
   const path = options.mtdPath
   const fileDescriptor = ob.fsOpen(path, 'w')
   const response = ob.requestBody(_.assign({}, options, {method: 'HEAD'}))
   const offset = response.pluck('message', 'headers', 'content-length').map(x => parseInt(x, 10))
-  const buffer = offset
-    .map(x => _.assign({}, options, {totalBytes: x}))
-    .map(x => u.toBuffer(x, options.maxBuffer))
+  const meta = offset.map(x => _.assign({}, options, {totalBytes: x}))
+  const buffer = meta.map(x => u.toBuffer(x, options.maxBuffer))
 
   return Rx.Observable.zip(
     fileDescriptor,
@@ -23,4 +23,6 @@ exports.create = (ob, options) => {
     offset,
     u.selectAs('fd', 'buffer', 'offset')
   ).flatMap(ob.fsWriteBuffer)
+    // TODO: add tests
+    .withLatestFrom(meta, (a, b) => b)
 }
