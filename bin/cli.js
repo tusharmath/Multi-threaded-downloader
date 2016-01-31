83f91430012eff53c629f4c70c24194156b80c0e
tushar mathur <tusharmath@gmail.com>
2016-01-31 21:47:15 +0530

    refactor(cli): extract download create logic from bin/mtd to bin/cli

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+const _ = require('lodash')
+const URL = require('url')
+const PATH = require('path')
+const Rx = require('rx')
+const VALID_URL = require('valid-url')
+const fileNameGenerator = x => _.last(URL.parse(x).pathname.split('/')) || Date.now()
+const normalizePath = path => PATH.resolve(__dirname, path)
+const pathGenerator = x => normalizePath(fileNameGenerator(x))
+
+module.exports = (createDownload, _params) => {
+  const params = Rx.Observable.just(_params).pluck('flags')
+  const newDownload = params
+    .filter(x => VALID_URL.isUri(x.url))
+    .map(x => x.path ? x : _.defaults(x, {path: pathGenerator(x.url)}))
+    .flatMap(x => createDownload(x).start())
+
+  const resumeDownloads = params
+    .skipWhile(x => x.url)
+    .map(x => _.assign({}, x, {mtdPath: normalizePath(x.file), path: normalizePath(x.file.replace('.mtd', ''))}))
+    .flatMap(x => createDownload(x).download())
+
+  return Rx.Observable.merge(newDownload, resumeDownloads)
+}
