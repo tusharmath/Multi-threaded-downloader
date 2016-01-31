d9dc9dca486f4efab4d66cc11c27b3a8d0cc0bb2
tushar mathur <tusharmath@gmail.com>
2016-01-31 21:51:58 +0530

    refactor(bin/cli): rename variables

--- bin/cli.js
+++ bin/cli.js
@@ -7,14 +7,14 @@ const fileNameGenerator = x => _.last(URL.parse(x).pathname.split('/')) || Date.
 const normalizePath = path => PATH.resolve(__dirname, path)
 const pathGenerator = x => normalizePath(fileNameGenerator(x))
 
-module.exports = (createDownload, _params) => {
-  const params = Rx.Observable.just(_params).pluck('flags')
-  const newDownload = params
+module.exports = (createDownload, params) => {
+  const pFlags = Rx.Observable.just(params.flags)
+  const newDownload = pFlags
     .filter(x => VALID_URL.isUri(x.url))
     .map(x => x.path ? x : _.defaults(x, {path: pathGenerator(x.url)}))
     .flatMap(x => createDownload(x).start())
 
-  const resumeDownloads = params
+  const resumeDownloads = pFlags
     .skipWhile(x => x.url)
     .map(x => _.assign({}, x, {mtdPath: normalizePath(x.file), path: normalizePath(x.file.replace('.mtd', ''))}))
     .flatMap(x => createDownload(x).download())
