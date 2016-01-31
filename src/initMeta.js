d6b54714dddd164f0eee2352422e11a856ccf37f
tushar mathur <tusharmath@gmail.com>
2016-01-31 16:23:34 +0530

    fix(initMeta): create meta of only selected properties

--- src/initMeta.js
+++ src/initMeta.js
@@ -2,10 +2,13 @@
 
 const _ = require('lodash')
 const splitRange = require('./splitRange')
-
+const PROPS = [
+  'range', 'url', 'totalBytes', 'threads', 'offsets', 'strictSSL'
+]
 module.exports = (ob, options) => ob
     .requestContentLength(options)
     .map(x => {
       const threads = splitRange(x, options.range)
       return _.assign({}, options, {totalBytes: x, threads, offsets: threads.map(x => x[0])})
     })
+    .map(x => _.pick(x, PROPS))
