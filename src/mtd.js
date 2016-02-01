8c7077edbae9de40b65521affa6bbdeec4f7b3a1
tushar mathur <tusharmath@gmail.com>
2016-02-01 14:38:50 +0530

    feat(mtd): expose all download events on stats property

--- src/mtd.js
+++ src/mtd.js
@@ -2,6 +2,8 @@
  * Created by tusharmathur on 5/15/15.
  */
 'use strict'
+const Rx = require('rx')
+const _ = require('lodash')
 const createFD = require('./createFD')
 const initMTD = require('./initMTD')
 const initParams = require('./initParams')
@@ -12,6 +14,9 @@ class Download {
     this.options = initParams(options)
     this.ob = ob
     this.fd = createFD(ob, this.options.mtdPath)
+    this.stats = new Rx.BehaviorSubject()
+    this.toStat = _.curry((event, message) => this.stats.onNext({event, message}))
+    this.toStat('INIT', this.options)
   }
 
   start () {
@@ -22,6 +27,7 @@ class Download {
 
   init () {
     return initMTD(this.ob, this.fd('w'), this.options)
+      .tap(this.toStat('CREATE'))
   }
 
   download () {
@@ -29,12 +35,15 @@ class Download {
     const options = this.options
     const ob = this.ob
     return downloadMTD(ob, fd)
+      .tap(this.toStat('DATA'))
       .last()
       .flatMap(x => ob.fsTruncate(options.mtdPath, x.totalBytes))
+      .tap(this.toStat('TRUNCATE'))
       .flatMap(() => ob.fsRename(options.mtdPath, options.path))
+      .tap(this.toStat('RENAME'))
+      .tapOnCompleted(x => this.stats.onCompleted(x))
   }
 
-  stop () {
-  }
+  stop () {}
 }
 module.exports = Download
