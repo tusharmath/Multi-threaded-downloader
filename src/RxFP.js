e898744add82c814776dce56afb7b2c2b64d9887
tushar mathur <tusharmath@gmail.com>
2016-06-11 16:53:56 +0530

    refactor(RxFP): add more rx operators

--- src/RxFP.js
+++ src/RxFP.js
@@ -5,11 +5,17 @@
 'use strict'
 
 import R from 'ramda'
+import {Observable as O} from 'rx'
 export const map = R.curry((func, $) => $.map(func))
+export const flatMap = R.curry((func, $) => $.flatMap(func))
 export const withLatestFrom = R.curry((list, $) => $.withLatestFrom(...list))
 export const zip = R.curry((list, $) => $.zip(...list))
+export const zipWith = R.curry((func, list, $) => $.zip(...list, func))
 export const filter = R.curry((func, $) => $.filter(func))
 export const distinctUntilChanged = $ => $.distinctUntilChanged()
 export const pluck = R.curry((path, $) => $.pluck(path))
 export const scan = R.curry((func, $) => $.scan(func))
+export const scanWith = R.curry((func, m, $) => $.scan(func, m))
 export const shareReplay = R.curry((count, $) => $.shareReplay(count))
+export const repeat = R.curry((value, count) => O.repeat(value, count))
+export const trace = R.curry((msg, $) => $.tap(x => console.log(msg, x)))
