d4bed24ad199808d8e1883eaeafa063b0020f1b1
tushar mathur <tusharmath@gmail.com>
2016-06-21 16:10:52 +0530

    refactor(index): remove Stats subject
    muxer should be used instead of stats
    
    BREAKING CHANGE: downloader.stats has been depricated, use demux() instead

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
  * Created by tusharmathur on 5/15/15.
  */
 'use strict'
-import Rx from 'rx'
-import R from 'ramda'
 import request from 'request'
 import {demux} from 'muxer'
 import fs from 'graceful-fs'
@@ -14,16 +12,13 @@ export const createDownload = (_options) => {
   const [HTTP] = T.HTTP(request)
   const [FILE] = T.FILE(fs)
   const options = MergeDefaultOptions(_options)
-  const stats = new Rx.BehaviorSubject({event: 'INIT', message: options})
-  const toStat = R.curry((event, message) => stats.onNext({event, message}))
-
   const start = () => {
     return init().flatMap(() => download())
   }
 
   const init = () => {
     const [{written$}] = demux(CreateMTDFile({FILE, HTTP, options}), 'written$')
-    return written$.tap(toStat('CREATE'))
+    return written$
   }
   const download = () => {
     const [{metaPosition$, fd$, meta$}] = demux(
